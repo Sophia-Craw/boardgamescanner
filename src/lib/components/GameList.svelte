c0193839b20b0c7610ec1c923ce4bfd38a36cee4
Soph <145169616+Sophia-Craw@users.noreply.github.com>
2025-12-10 08:20:45 -0600

    implemented search

--- src/lib/components/GameList.svelte
+++ src/lib/components/GameList.svelte
@@ -4,9 +4,10 @@
     import RightArrow from "$lib/assets/right-arrow.png";
     import { wishlistList } from "$lib/stores";
     import type { GameObject } from "$lib/types";
-    import { onMount } from "svelte";
+    import SearchIcon from "$lib/assets/magnifying-glass.png"
 
     let wishlist: Array<GameObject> = $state([]);
+    let searchTerm: string = $state("");
 
     $effect(() => {
         wishlistList.subscribe((list) => {
@@ -18,41 +19,49 @@
 </script>
 
 <main>
-    <div class="tabs">
-        <button
-            class={view == "collection" ? "tab-select" : "tab"}
-            onclick={() => {
-                view = "collection";
-            }}>Collection</button
-        >
-        <button
-            class={view == "wishlist" ? "tab-select" : "tab"}
-            onclick={() => {
-                view = "wishlist";
-            }}>Wishlist</button
-        >
+    <div class="control-group">
+        <div class="search-bar">
+            <img class="search-icon" src={SearchIcon} alt="">
+            <input type="text" class="txt-search" placeholder="Search..." bind:value={searchTerm}>
+        </div>
+        <div class="tabs">
+            <button
+                class={view == "collection" ? "tab-select" : "tab"}
+                onclick={() => {
+                    view = "collection";
+                }}>Collection</button
+            >
+            <button
+                class={view == "wishlist" ? "tab-select" : "tab"}
+                onclick={() => {
+                    view = "wishlist";
+                }}>Wishlist</button
+            >
+        </div>
     </div>
     {#each view === "collection" ? games : wishlist as game}
-        <a href="/result/{game.upc}">
-            <div class="game">
-                <div class="info-wrapper">
-                    <div class="thumbnail-wrapper">
-                        <img
-                            class="thumbnail"
-                            src={game.bgg_info[0].image_url}
-                            alt=""
-                        />
+        {#if game.name.includes(searchTerm) || game.published === searchTerm}
+            <a href="/result/{game.upc}">
+                <div class="game">
+                    <div class="info-wrapper">
+                        <div class="thumbnail-wrapper">
+                            <img
+                                class="thumbnail"
+                                src={game.bgg_info[0].image_url}
+                                alt=""
+                            />
+                        </div>
+                        <div class="game-info">
+                            <p class="title">{game.name}</p>
+                            <p class="year">{game.bgg_info[0].published}</p>
+                        </div>
                     </div>
-                    <div class="game-info">
-                        <p class="title">{game.name}</p>
-                        <p class="year">{game.bgg_info[0].published}</p>
+                    <div class="arrow-wrapper">
+                        <img class="icon" src={RightArrow} alt="" />
                     </div>
                 </div>
-                <div class="arrow-wrapper">
-                    <img class="icon" src={RightArrow} alt="" />
-                </div>
-            </div>
-        </a>
+            </a>           
+        {/if}
     {/each}
     {#if games.length > 0}{:else}
         <p>No games yet.</p>
@@ -60,6 +69,13 @@
 </main>
 
 <style>
+    .control-group {
+        display: flex;
+        flex-direction: column;
+        gap: 12px;
+        padding: 12px;
+    }
+
     .game {
         display: flex;
         justify-content: space-between;
@@ -71,10 +87,17 @@
         transition: 0.08s;
     }
 
-    .tabs {
+    .search-bar {
         display: flex;
         gap: 12px;
         padding: 10px;
+        background-color: rgb(27, 27, 27);
+        border-radius: 12px;
+    }
+
+    .tabs {
+        display: flex;
+        gap: 12px;
     }
 
     .tab {
@@ -162,6 +185,32 @@
         filter: invert() opacity(50%);
         position: relative;
         top: 14px;
+    }
+
+    .search-icon {
+        width: 12px;
+        height: 12px;
+        filter: invert() opacity(50%);
+        position: relative;
+        top: 3px;
+        transition: 0.08s;
+    }
+
+    .txt-search {
+        background-color: transparent;
+        border: none;
+        outline: none;
+        color: white;
+    }
+
+    .search-bar {
+        transition: 0.08s;
+    }
+
+    .search-bar:has(.txt-search:focus) {
+        outline-style: solid;
+        outline-width: 4px;
+        outline-color: rgba(138, 189, 255, 0.473);
     }
 
     a {
